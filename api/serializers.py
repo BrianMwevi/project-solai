660a209c487d1762533629d8e649b68bcec9078e
brianmwevi <mwevibrian@gmail.com>
2022-06-25 15:45:06 +0300

    ft:stock -- add stock serializer

---
+++ api/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from stocks_v1.models import Stock
+
+
+class StockSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Stock
+        fields = [
+            'id',
+            'category',
+            'name',
+            'ticker',
+            'volume',
+            'price',
+            'prev_price',
+            'open_price',
+            'change',
+            'max_price',
+            'min_price',
+            'updated_at',
+        ]
