d706968a7cd3d62cd0a81f92bf92caf74594400f
brianmwevi <mwevibrian@gmail.com>
2022-06-25 15:46:51 +0300

    ft:routes -- add stock detail route

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ from api.views import StockDetail, StockList
 
 urlpatterns = [
     path('stocks/', StockList.as_view(), name='stock-list'),
+    path('stocks/<int:id>/', StockDetail.as_view(), name='stock-detail'),
 ]
