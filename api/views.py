663773207fc366985c4104243f5df3806812bc5d
brianmwevi <mwevibrian@gmail.com>
2022-06-25 15:46:03 +0300

    ft:stock detail -- add stock detail view

--- api/views.py
+++ api/views.py
@@ -6,3 +6,9 @@ from stocks_v1.models import Stock
 class StockList(generics.ListAPIView):
     serializer_class = StockSerializer
     queryset = Stock.objects.all()
+
+
+class StockDetail(generics.RetrieveAPIView):
+    serializer_class = StockSerializer
+    queryset = Stock.objects.all()
+    lookup_field = 'id'
