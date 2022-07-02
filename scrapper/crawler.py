ad07d3ce4ceabcf06ecbb58d35327341c765a8a0
brianmwevi <mwevibrian@gmail.com>
2022-07-02 16:00:23 +0300

    ft:parse data -- implements bs4 to parse fetched data

--- scrapper/crawler.py
+++ scrapper/crawler.py
@@ -8,9 +8,15 @@ from decouple import config
 
 
 async def fetch_url(url: str, session: ClientSession, **kwargs):
+    print("Start: ", datetime.now())
     resp = await session.request(method='GET', url=url, **kwargs)
     resp.raise_for_status()
     xml_data = await resp.text()
     return xml_data
 
 
+async def parse_data():
+    async with ClientSession() as session:
+        xml_data = await fetch_url(config("URL_V1"), session)
+        soup = BeautifulSoup(xml_data, features="xml")
+        return soup.select("i")
