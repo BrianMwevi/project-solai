8f4b71c1575fab7293539ca91da67559267a52bd
brianmwevi <mwevibrian@gmail.com>
2022-07-21 21:32:47 +0300

    patch: stock view -- remove zombie code

--- api/views.py
+++ api/views.py
@@ -1,19 +1,10 @@
 from api.serializers import StockSerializer
 from rest_framework import generics, viewsets
 from stocks_v1.models import Stock
+from rest_framework import serializers, status
+from rest_framework.response import Response
 
 
 class StockViewset(viewsets.ModelViewSet):
     serializer_class = StockSerializer
     queryset = Stock.objects.all()
-
-
-# class StockList(generics.ListAPIView):
-#     serializer_class = StockSerializer
-#     queryset = Stock.objects.all()
-
-
-# class StockDetail(generics.RetrieveAPIView):
-#     serializer_class = StockSerializer
-#     queryset = Stock.objects.all()
-#     lookup_field = 'id'
