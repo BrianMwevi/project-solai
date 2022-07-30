f7a65b38ee54a36589a75440a2393b9e7855140b
brianmwevi <mwevibrian@gmail.com>
2022-07-30 18:25:17 +0300

    ft:stocks crud -- add url for create and update api view

--- api/urls.py
+++ api/urls.py
@@ -2,14 +2,13 @@ from django.urls import path
 
 from rest_framework.routers import DefaultRouter
 
-from api.views import StockViewset
+from api.views import StockUpdateView, StockViewset
 
 routes = DefaultRouter()
 
 routes.register('stocks', StockViewset, basename='stock')
 urlpatterns = [
-    # path('stocks/', StockList.as_view(), name='stock-list'),
-    # path('stocks/<int:id>/', StockDetail.as_view(), name='stock-detail'),
+    path('stocks/crud/', StockUpdateView.as_view()),
 ]
 
 urlpatterns += routes.urls
