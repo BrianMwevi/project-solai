b28559331744f4ee6a75f35d85e8bcd3f62e8ef1
brianmwevi <mwevibrian@gmail.com>
2022-08-06 11:31:45 +0300

    ch:admin dashboard -- register Investor, Trader, Developer and Admin models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from accounts.models import User
+from accounts.models import User, Admin, Investor, Trader
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Trader)
+admin.site.register(Investor)
+admin.site.register(Admin)
