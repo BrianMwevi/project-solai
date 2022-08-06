551b9d0a5db40945e42cc7ea972a50432909c49e
brianmwevi <mwevibrian@gmail.com>
2022-08-06 11:34:32 +0300

    ft:BaseThrottle -- add Investor bse throttling

--- accounts/throttles.py
+++ accounts/throttles.py
@@ -9,4 +9,11 @@ class DeveloperThrottle(UserRateThrottle):
     scope = 'trader'
 
 
-
+class InvestorThrottle(BaseThrottle):
+    def allow_request(self, request, view):
+        whitelist = "127.0.0.1:8000"
+        incoming = request.META.get("HTTP_HOST")
+        print(whitelist, incoming)
+        if request.user.role == 'INVESTOR' and whitelist == incoming:
+            return True
+        return False
