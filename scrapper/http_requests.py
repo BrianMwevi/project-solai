20b8baec7f7ffb4c2a586a5cfd1d621e00771cab
brianmwevi <mwevibrian@gmail.com>
2022-08-13 02:11:19 +0300

    ft:websockets -- send group message function

--- scrapper/http_requests.py
+++ scrapper/http_requests.py
@@ -1,6 +1,7 @@
 import json
 from decouple import config
 from aiohttp import ClientSession
+from channels.layers import get_channel_layer
 
 
 def get_url():
@@ -8,12 +9,18 @@ def get_url():
     return api_url
 
 
+async def fetch_data(url: str, method: str, session: ClientSession, data, **kwargs):
+    resp = await session.request(method=method, url=url, json=data, **kwargs)
+    resp.raise_for_status()
+    return json.loads(await resp.text())
+
+
 async def create_stocks(create_list):
     custom_print(f"Creating: {len(create_list)} stock(s)...")
     url = get_url()
     data = {"stocks": create_list}
     async with ClientSession() as session:
-        stocks = await fetch_url(url, 'POST', session, data)
+        stocks = await fetch_data(url, 'POST', session, data)
         custom_print(f"Created: {len(stocks['stocks'])} stock(s)\n")
         return stocks
 
@@ -23,15 +30,20 @@ async def update_stocks(update_list):
     url = get_url()
     data = {"stocks": update_list}
     async with ClientSession() as session:
-        stocks = await fetch_url(url, 'PUT', session, data)
+        stocks = await fetch_data(url, 'PUT', session, data)
         custom_print(f"Updated: {len(stocks['stocks'])} stock(s)\n")
         return stocks
 
 
-async def fetch_url(url: str, method: str, session: ClientSession, data, **kwargs):
-    resp = await session.request(method=method, url=url, json=data, **kwargs)
-    resp.raise_for_status()
-    return json.loads(await resp.text())
+async def update_clients(stock):
+    channel_layer = get_channel_layer()
+    await channel_layer.group_send(
+        'stock_clients',
+        {
+            'type': 'client_message',
+            'data': stock,
+        }
+    )
 
 
 def custom_print(text):
