acbc7dfef2689db5670e3bd08926b09ea65a838a
brianmwevi <mwevibrian@gmail.com>
2022-08-13 02:16:05 +0300

    ft:websockets -- add app configs

---
+++ stocks_channel/consumers.py
@@ -0,0 +1,20 @@
+# chat/consumers.py
+from ipaddress import ip_address
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+
+class StockConsumer(AsyncWebsocketConsumer):
+
+    async def connect(self):
+        await self.channel_layer.group_add("stock_clients", self.channel_name)
+
+        await self.accept()
+
+    async def disconnect(self, code):
+        self.channel_layer.group_discard(
+            "stock_clients", self.channel_name)
+
+    async def client_message(self, event):
+        data = event['data']
+        await self.send(text_data=json.dumps(data))
