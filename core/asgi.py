7a175b972689073f924411f23b8aa421101a7677
brianmwevi <mwevibrian@gmail.com>
2022-08-13 02:08:15 +0300

    ft:websockets -- add asgi configurations

--- core/asgi.py
+++ core/asgi.py
@@ -1,7 +1,17 @@
 import os
-
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+import stocks_channel.routing
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(AuthMiddlewareStack(URLRouter(
+        stocks_channel.routing.websocket_urlpatterns
+    )))
+})
