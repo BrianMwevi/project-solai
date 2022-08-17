beb72bf8e03126a4cb8719e50691ac72f32ac2be
brianmwevi <mwevibrian@gmail.com>
2022-08-17 17:17:48 +0300

    patch:send -- send stocks upon client connection acceptance

--- stocks_channel/consumers.py
+++ stocks_channel/consumers.py
@@ -2,14 +2,17 @@
 from ipaddress import ip_address
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from scrapper.updater import get_stocks
 
 
 class StockConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         await self.channel_layer.group_add("stock_clients", self.channel_name)
-
         await self.accept()
+
+        stocks = get_stocks()
+        await self.send(text_data=json.dumps(list(stocks.values())))
 
     async def disconnect(self, code):
         await self.channel_layer.group_discard(
