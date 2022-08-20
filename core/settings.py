276d4a6889330835f8a73248c87e53c134438f78
brianmwevi <mwevibrian@gmail.com>
2022-08-20 20:29:25 +0300

    ch:react -- add react port in allowed origins

--- core/settings.py
+++ core/settings.py
@@ -161,7 +161,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
 
-    "http://localhost:4200",
+    "http://localhost:3000",
     "http://127.0.0.1:8000",
     "https://open-stocks.herokuapp.com",
     "https://brianmwevi.github.io",
