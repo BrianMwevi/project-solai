13aef8621c2b3fb239165c1b94a1a71c829bb5cb
brianmwevi <mwevibrian@gmail.com>
2022-09-17 07:31:53 +0300

    ch:cors -- add railway app

--- core/settings.py
+++ core/settings.py
@@ -162,7 +162,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
     "https://open-stocks.herokuapp.com",
-    # "https://web-production-7794.up.railway.app",
+    "https://web-production-7794.up.railway.app",
     "https://brianmwevi.github.io",
 ]
 
