8ff1c16fb0f18665b084498e5e8281c9ae7767b5
brianmwevi <mwevibrian@gmail.com>
2022-09-17 14:23:24 +0300

    fix:asgi -- remove permission restriction

--- core/asgi.py
+++ core/asgi.py
@@ -1,3 +1,4 @@
+import stocks_channel.routing
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
@@ -7,11 +8,13 @@ from channels.auth import AuthMiddlewareStack
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 django_asgi_app = get_asgi_application()
 
-import stocks_channel.routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AllowedHostsOriginValidator(AuthMiddlewareStack(URLRouter(
+    # "websocket": AllowedHostsOriginValidator(AuthMiddlewareStack(URLRouter(
+    #     stocks_channel.routing.websocket_urlpatterns
+    # )))
+    "websocket": URLRouter(
         stocks_channel.routing.websocket_urlpatterns
-    )))
+    )
 })
