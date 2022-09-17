8148ec70d954ce55ac05eecb06dcd21f0f466c31
brianmwevi <mwevibrian@gmail.com>
2022-09-17 16:25:37 +0300

    fix:channels

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: daphne core.asgi:application --port $PORT --bind 0.0.0.0 -v2
+web: daphne core.asgi:application 0.0.0.0:$PORT
 worker: python manage.py runworker channels --settings=core.settings -v2
--- core/asgi.py
+++ core/asgi.py
@@ -1,23 +1,20 @@
+import stocks_channel.routing
 import os
-from django.urls import re_path
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
-from stocks_channel import consumers
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-# django_asgi_app = get_asgi_application()
+django_asgi_app = get_asgi_application()
 
-# import stocks_channel.routing
 
 application = ProtocolTypeRouter({
     # "http": django_asgi_app,
     # "websocket": AllowedHostsOriginValidator(AuthMiddlewareStack(URLRouter(
     #     stocks_channel.routing.websocket_urlpatterns
     # )))
-    "websocket": URLRouter([
-        re_path(r'ws/stocks/realtime/$', consumers.StockConsumer.as_asgi()),
-
-    ])
+    "websocket": URLRouter(
+        stocks_channel.routing.websocket_urlpatterns
+    )
 })
