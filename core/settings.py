24a4c0f90bc7f02630d51eca38d792ff78bac2c1
brianmwevi <mwevibrian@gmail.com>
2022-09-17 18:28:39 +0300

    ch:add staticfiles

--- core/settings.py
+++ core/settings.py
@@ -1,7 +1,7 @@
+import dj_database_url
 from decouple import config, Csv
 from pathlib import Path
 import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,7 +149,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
