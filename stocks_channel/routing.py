d6cf1101a691cdfb41067e699d885af1b454a71f
brianmwevi <mwevibrian@gmail.com>
2022-09-17 19:38:51 +0300

    ch:add staticfiles

--- stocks_channel/routing.py
+++ stocks_channel/routing.py
@@ -3,5 +3,4 @@ from stocks_channel import consumers
 
 
 websocket_urlpatterns = [
-    re_path(r'ws/stocks/realtime/$', consumers.StockConsumer.as_asgi()),
-]
+    re_path(r'ws/stocks/realtime/$', consumers.StockConsumer), ]
