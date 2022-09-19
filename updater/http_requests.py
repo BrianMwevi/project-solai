5a402e8809453e2963ec0c0701f959536c49560c
brianmwevi <mwevibrian@gmail.com>
2022-09-19 09:53:45 +0300

    patch:sockets -- send message when stocks changes

--- updater/http_requests.py
+++ updater/http_requests.py
@@ -1,6 +1,7 @@
 import json
 from decouple import config
 from aiohttp import ClientSession
+from channels.layers import get_channel_layer
 
 
 class StocksController:
@@ -27,6 +28,12 @@ class StocksController:
             cls.logger(
                 f"{operation} {len(stocks['stocks'])} stock(s)\n")
             return stocks
+
+    async def update_clients(data):
+        channel_layer = get_channel_layer()
+        await channel_layer.group_send(
+            {"type": "client_message", data: data}
+        )
 
     def logger(text):
         return print(f"\n::: {text}", end="")
