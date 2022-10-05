c558c3af3bbd3b12be299cfc86395c2a62fd1ea3
brianmwevi <mwevibrian@gmail.com>
2022-10-05 11:42:58 +0300

    ft:consumer -- add notifications feature

--- stocks_channel/consumers.py
+++ stocks_channel/consumers.py
@@ -4,10 +4,10 @@ from updater.compare import get_stocks
 
 
 class StockConsumer(AsyncWebsocketConsumer):
-
+    """Handles connections for unauthenticated users/clients"""
     async def connect(self):
-        await self.channel_layer.group_add("stock_clients", self.channel_name)
         await self.accept()
+        await self.channel_layer.group_add("stock_clients", self.channel_name)
 
         stocks = list(get_stocks().values())
         await self.send(text_data=json.dumps(stocks))
@@ -17,5 +17,24 @@ class StockConsumer(AsyncWebsocketConsumer):
             "stock_clients", self.channel_name)
 
     async def client_message(self, event):
+        data = event['data']
+        await self.send(text_data=json.dumps(data))
+
+
+class NotificationConsumer(AsyncWebsocketConsumer):
+    """Handles websocket connections for authenticated users/clients and sends notifications"""
+
+    async def connect(self):
+        user = self.scope["user"]
+        if not user.is_authenticated:
+            return await self.close()
+
+        await self.accept()
+        await self.channel_layer.group_add("stock_users", self.channel_name)
+
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard("stock_users", self.channel_name)
+
+    async def send_notification(self, event):
         data = event['data']
         await self.send(text_data=json.dumps(data))
--- stocks_channel/routing.py
+++ stocks_channel/routing.py
@@ -4,4 +4,6 @@ from stocks_channel import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/stocks/realtime/$', consumers.StockConsumer.as_asgi()),
+    re_path(r'ws/stocks/notifications/$',
+            consumers.NotificationConsumer.as_asgi()),
 ]
