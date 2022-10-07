4b0fbb6f359f44d49ef60acda6c063920993aa6e
brianmwevi <mwevibrian@gmail.com>
2022-10-07 09:13:29 +0300

    ft:user -- add auth user to stock tracker group

--- stocks_channel/consumers.py
+++ stocks_channel/consumers.py
@@ -2,15 +2,22 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from updater.compare import get_stocks
 
+# TODO: Move notifications code to Notificatin class
+
 
 class StockConsumer(AsyncWebsocketConsumer):
     """Handles connections for unauthenticated users/clients"""
     async def connect(self):
         await self.accept()
-        await self.channel_layer.group_add("stock_clients", self.channel_name)
+        user = self.scope['user']
 
-        stocks = list(get_stocks().values())
-        await self.send(text_data=json.dumps(stocks))
+        if user.is_authenticated:
+            [await self.channel_layer.group_add(f"{tracker.ticker}{tracker.quote_price}") for tracker in user.trackers.all()]
+            print("Trackers: ", user.trackers.all())
+        else:
+            await self.channel_layer.group_add("stock_clients", self.channel_name)
+            stocks = list(get_stocks().values())
+            await self.send(text_data=json.dumps(stocks))
 
     async def disconnect(self, code):
         await self.channel_layer.group_discard(
@@ -19,6 +26,10 @@ class StockConsumer(AsyncWebsocketConsumer):
     async def client_message(self, event):
         data = event['data']
         await self.send(text_data=json.dumps(data))
+
+    async def notification(self, event):
+        data = event['data']
+        return await self.send(text_data=data)
 
 
 class NotificationConsumer(AsyncWebsocketConsumer):
@@ -31,6 +42,8 @@ class NotificationConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
         await self.channel_layer.group_add("stock_users", self.channel_name)
+
+    # TODO: Add receive method when user opens a notification
 
     async def disconnect(self, code):
         await self.channel_layer.group_discard("stock_users", self.channel_name)
