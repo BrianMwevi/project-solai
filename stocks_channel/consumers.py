5d06f778733e6225d4d980677c1a1c92217324f6
brianmwevi <mwevibrian@gmail.com>
2022-10-08 08:34:45 +0300

    ft:stocks -- add history websocket channel

--- stocks_channel/consumers.py
+++ stocks_channel/consumers.py
@@ -1,11 +1,14 @@
 import json
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer, AsyncJsonWebsocketConsumer
 from updater.compare import get_stocks
+from channels.db import database_sync_to_async
+from stocks_v1.models import Stock, StockTracker
+from api.serializers import StockSerializer
 
 # TODO: Move notifications code to Notificatin class
 
 
-class StockConsumer(AsyncWebsocketConsumer):
+class StockConsumer(AsyncJsonWebsocketConsumer):
     """Handles connections for unauthenticated users/clients"""
     async def connect(self):
         await self.accept()
@@ -32,7 +35,7 @@ class StockConsumer(AsyncWebsocketConsumer):
         return await self.send(text_data=data)
 
 
-class NotificationConsumer(AsyncWebsocketConsumer):
+class NotificationConsumer(AsyncJsonWebsocketConsumer):
     """Handles websocket connections for authenticated users/clients and sends notifications"""
 
     async def connect(self):
@@ -44,10 +47,31 @@ class NotificationConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_add("stock_users", self.channel_name)
 
     # TODO: Add receive method when user opens a notification
-
     async def disconnect(self, code):
         await self.channel_layer.group_discard("stock_users", self.channel_name)
 
     async def send_notification(self, event):
         data = event['data']
         await self.send(text_data=json.dumps(data))
+
+
+class HistoryConsumer(AsyncWebsocketConsumer):
+    """Handles requests for historical data of a specific stock"""
+
+    async def connect(self):
+        if self.scope['user'].is_authenticated:
+            await self.accept()
+        else:
+            # await self.close() # uncomment this line to enable user auth
+            await self.accept() # remove this line to enable user auth
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        ticker = text_data_json['ticker'].upper()
+        data = await self.get_history(ticker)
+        await self.send(text_data=json.dumps(data))
+
+    @database_sync_to_async
+    def get_history(self, ticker):
+        serializer = StockSerializer(Stock.get_history(ticker), many=True)
+        return {'data': serializer.data}
