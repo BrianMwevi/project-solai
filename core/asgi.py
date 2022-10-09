cba99d113afd380c0c151f586fa7381443c2a6f8
brianmwevi <mwevibrian@gmail.com>
2022-10-09 09:20:37 +0300

    ft:channels -- add user authentication middleware

--- core/asgi.py
+++ core/asgi.py
@@ -1,17 +1,19 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
 import stocks_channel.routing
 import os
 from django.core.asgi import get_asgi_application
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
+from stocks_channel.userAuthMiddleware import UserAuthMiddleware
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 django_asgi_app = get_asgi_application()
 
-from channels.routing import ProtocolTypeRouter, URLRouter
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AllowedHostsOriginValidator(AuthMiddlewareStack(URLRouter(
+    "websocket": UserAuthMiddleware(URLRouter(
         stocks_channel.routing.websocket_urlpatterns
-    )))
+    ))
 })
