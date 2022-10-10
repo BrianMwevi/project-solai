8a25d36a9fc2b9034b3c65363dd6a02ca72f9b83
brianmwevi <mwevibrian@gmail.com>
2022-10-10 11:42:59 +0300

    fix:app registry -- import settings after app has been loaded

--- core/asgi.py
+++ core/asgi.py
@@ -1,4 +1,3 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
 import stocks_channel.routing
 import os
 from django.core.asgi import get_asgi_application
@@ -10,6 +9,7 @@ from stocks_channel.userAuthMiddleware import UserAuthMiddleware
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 django_asgi_app = get_asgi_application()
 
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
