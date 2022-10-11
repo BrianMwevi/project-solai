f9450425dc712b6daf884c73d29deff6908756e6
brianmwevi <mwevibrian@gmail.com>
2022-10-11 11:34:54 +0300

    patch:static -- add setting in core urls

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,10 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
-
-
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
-
-    
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
