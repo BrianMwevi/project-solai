f45e1e12cd9d7e2cab2c1dcf15278459de9e21d9
brianmwevi <mwevibrian@gmail.com>
2022-10-31 13:08:16 +0300

    ch:add email settings

--- core/settings.py
+++ core/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -153,6 +153,14 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# EMAIL SETTINGS
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('ADMIN_EMAIL')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
 
 # WHITELISTED DOMAINS TO ACCESS THIS APP
 CORS_ALLOWED_ORIGINS = [
--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('accounts.urls')),
     # path('api/v1/', include('api.urls')),
     # path('', include('api.urls')),
 ]
