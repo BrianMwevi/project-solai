dcf92bf98e5be277a92f999cf7e428bb9c0f2fd1
brianmwevi <mwevibrian@gmail.com>
2022-11-01 12:53:17 +0300

    ft:add can activate api key reset and generation permissions

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -1,6 +1,18 @@
 from rest_framework import permissions
 
 
+class CanReset(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.api_key is not None
+
+
+class CanGenerate(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.api_key is None
+
+
 class IsAdmin(permissions.BasePermission):
 
     def has_permission(self, request, view):
