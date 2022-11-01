3b6d7af3ac6bca40182ac192fa5de46314063fbb
brianmwevi <mwevibrian@gmail.com>
2022-11-01 12:55:10 +0300

    ft:add api key reset feature

--- accounts/api_keys.py
+++ accounts/api_keys.py
@@ -1,26 +1,27 @@
-from calendar import month
 from rest_framework_api_key.models import APIKey
-from datetime import datetime, timedelta
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class NewKey:
 
     @classmethod
     def generate(cls, user_id):
-        expiry_date = None
-        try:
-            api_key = APIKey.objects.get(name=user_id)
-            if api_key.has_expired:
-                expiry_date = datetime.now() + timedelta(days=30)
-            else:
-                expiry_date = api_key.expiry_date
-            api_key.delete()
+        api_key, key = APIKey.objects.create_key(name=user_id)
+        user = cls.add_to_user(user_id, api_key)
+        return (api_key, key)
 
-        except APIKey.DoesNotExist:
-            expiry_date = datetime.now() + timedelta(days=30)
+    @classmethod
+    def reset(cls, user_id):
+        user_key = User.get_user(user_id).api_key
+        old_api_key = APIKey.objects.get(name=user_key.name)
+        old_api_key.delete()
+        return cls.generate(user_id)
 
-        finally:
-            api_key, key = APIKey.objects.create_key(name=user_id)
-            api_key.expiry_date = expiry_date
-            api_key.save()
-        return (key, api_key.expiry_date)
+    def add_to_user(user_id, api_key):
+        user = User.get_user(user_id)
+        user.api_key = api_key
+        user.save()
+        return user
+
