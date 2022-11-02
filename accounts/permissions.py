9e84d0134376b5235f7269137d42fb28940d6dd1
brianmwevi <mwevibrian@gmail.com>
2022-11-03 01:12:26 +0300

    fix:remove CanReset persmission

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -1,12 +1,6 @@
 from rest_framework import permissions
 
 
-class CanReset(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return request.user.api_key is not None
-
-
 class CanGenerate(permissions.BasePermission):
 
     def has_permission(self, request, view):
