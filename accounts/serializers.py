bba28bd2311a570c2586874df6e9c320cf330167
brianmwevi <mwevibrian@gmail.com>
2022-11-03 01:18:24 +0300

    fix:change model from Developer to User

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,16 +1,13 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from emailer.confirmation_email import EmailConfirmation
-from accounts.models import Developer, Trader, Investor, Admin
-from asgiref.sync import async_to_sync
-from rest_framework_api_key.models import APIKey
 User = get_user_model()
 
 
 class DeveloperSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Developer
+        model = User
         fields = ['id', 'username', 'email', 'password', 'usage', 'role']
         extra_kwargs = {'password': {'write_only': True},
                         }
@@ -25,6 +22,9 @@ class DeveloperSerializer(serializers.ModelSerializer):
         user = User(email=email, username=username, usage=usage, role=role)
         user.set_password(password)
         user.save()
-        sent_email = async_to_sync(
-            EmailConfirmation.signup)(self.context['request'], user)
+
+        mail_subject = "Solai Account Activation"
+        template = 'accounts/account_activation_email.html'
+        _ = EmailConfirmation.email_user(
+            self.context['request'], user, mail_subject, template)
         return user
