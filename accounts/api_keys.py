56045a84abcaa2ac64bbc7186792ed72fdc873d5
brianmwevi <mwevibrian@gmail.com>
2022-11-03 17:29:36 +0300

    patch:add expiry date to ApiKeys

--- accounts/api_keys.py
+++ accounts/api_keys.py
@@ -1,5 +1,6 @@
 from rest_framework_api_key.models import APIKey
 from django.contrib.auth import get_user_model
+from datetime import datetime, timedelta
 
 User = get_user_model()
 
@@ -9,7 +10,8 @@ class NewKey:
     @classmethod
     def generate(cls, user_id):
         api_key, key = APIKey.objects.create_key(name=user_id)
-        user = cls.add_to_user(user_id, api_key)
+        api_key.expiry_date = datetime.now() + timedelta(days=30)
+        cls.add_to_user(user_id, api_key)
         return (api_key, key)
 
     @classmethod
@@ -23,5 +25,4 @@ class NewKey:
         user = User.get_user(user_id)
         user.api_key = api_key
         user.save()
-        return user
 
