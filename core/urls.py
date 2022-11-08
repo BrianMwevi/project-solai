8ee20cdf7ba4a0440336e0844adfb3cffd267aae
brianmwevi <mwevibrian@gmail.com>
2022-11-08 16:36:11 +0300

    patch:change prefix from accounts to auth

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v2/', include('accounts.urls')),
+    path('auth/', include('accounts.urls')),
+    path('stocks/', include('stocks.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$',
             schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger',
