ffba621d74031f2cafcc4d23daacf92021887472
brianmwevi <mwevibrian@gmail.com>
2022-11-08 16:38:05 +0300

    ft:add stock serializer

---
+++ stocks/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from stocks.models import Stock
+
+
+class StockSerializer(serializers.ModelSerializer):
+    # id = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Stock
+        fields = "__all__"
+
+    # def get_id(self, obj):
+    #     return obj.pk
