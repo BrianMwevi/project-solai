167a4c0fb7555e6ba14d60263b6aa5a2983887df
brianmwevi <mwevibrian@gmail.com>
2022-11-08 16:30:45 +0300

    ft:add stockAdmin permission

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -1,4 +1,10 @@
+from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.auth import get_user_model
+
 from rest_framework import permissions
+from rest_framework_api_key.models import APIKey
+
+User = get_user_model()
 
 
 class CanGenerateKey(permissions.BasePermission):
@@ -11,6 +17,20 @@ class IsAdmin(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return perform_check(request, "ADMIN")
+
+
+class IsStockAdmin(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        key = request.META["HTTP_AUTHORIZATION"].split()[1]
+        api_key = APIKey.objects.get_from_key(key)
+        user = User.objects.filter(api_key=api_key).first()
+        # TODO: Verify request is from the same domain
+        # current_site = get_current_site(request)
+        # origin = request.META['REMOTE_ADDR']
+        # print("Domain: ", current_site.name)
+        # print("Origin: ", origin)
+        return user is not None and user.role == 'ADMIN'
 
 
 class IsInvestor(permissions.BasePermission):
--- stocks/views.py
+++ stocks/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
+from rest_framework import generics, views, status
+from rest_framework.response import Response
+from simple_history.utils import update_change_reason
+from rest_framework_api_key.permissions import HasAPIKey
 
-# Create your views here.
+from stocks.models import Stock
+from stocks.serializers import StockSerializer
+from accounts.permissions import IsStockAdmin
+
+
+class RealTimeStocks(generics.ListAPIView):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    # permission_classes = [HasAPIKey, IsAdmin |IsInvestor | IsTrader | IsDeveloper]
+
+    # def get_throttles(self):
+    #     throttle_classes = []
+    #     if self.request.user.role == "DEVELOPER":
+    #         throttle_classes = [DeveloperThrottle]
+    #     elif self.request.user.role == "INVESTOR":
+    #         throttle_classes = [InvestorThrottle]
+    #     elif self.request.user.role == "TRADER":
+    #         throttle_classes = [TraderThrottle]
+    #     return [throttle() for throttle in throttle_classes]
+
+
+class HistoryView(generics.ListAPIView):
+    queryset = Stock.history.all()
+    serializer_class = StockSerializer
+    # permission_classes = [IsAuthenticated, IsAdmin |
+    #                       IsInvestor | IsTrader | IsDeveloper]
+
+    # def get_throttles(self):
+    #     throttle_classes = []
+    #     if self.request.user.role == "DEVELOPER":
+    #         throttle_classes = [DeveloperThrottle]
+    #     elif self.request.user.role == "INVESTOR":
+    #         throttle_classes = [InvestorThrottle]
+    #     elif self.request.user.role == "TRADER":
+    #         throttle_classes = [TraderThrottle]
+    #     return [throttle() for throttle in throttle_classes]
+
+    def get_queryset(self):
+        return self.queryset.filter(ticker=self.request.GET.get('ticker'))[:30]
+
+
+class AdminApiView(views.APIView):
+    permission_classes = [HasAPIKey, IsStockAdmin]
+
+    def post(self, request):
+        stocks = request.data['stocks']
+        created_stocks = {"stocks": []}
+        for stock in stocks:
+            serializer = StockSerializer(data=stock)
+            if serializer.is_valid():
+                created_stock = serializer.save()
+                created_stocks['stocks'].append(serializer.data)
+                update_change_reason(created_stock, "Genesis Stock")
+
+        return Response(created_stocks, status=status.HTTP_201_CREATED)
+
+    def put(self, request, format=None):
+        stocks = request.data['stocks']
+        updated_stocks = {"stocks": []}
+        queryset = Stock.objects.all()
+        for stock in stocks:
+            instance = queryset.get(ticker=stock['ticker'])
+            serializer = StockSerializer(
+                instance, data=stock, context={'request': request})
+            if stock['change'] != float(instance.change) and serializer.is_valid():
+                updated_stock = serializer.save()
+                updated_stocks['stocks'].append(serializer.data)
+                update_change_reason(updated_stock, "Update")
+        return Response(updated_stocks)
