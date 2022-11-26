f5deac59b3469aabc23c76f5cba7cbf62fe39c5a
brianmwevi <mwevibrian@gmail.com>
2022-11-26 04:32:46 +0300

    fix:change close hour to 1600hrs

--- clock.py
+++ clock.py
@@ -7,12 +7,12 @@ scheduler = background.BackgroundScheduler()
 
 def start():
     scheduler.add_job(main, 'cron',
-                      day_of_week='mon-fri', hour='9-16', second='*/5', id="spider")
+                      day_of_week='mon-fri', hour='9-15', second='*/5', id="spider")
     scheduler.start()
 
 
 def market_is_open() -> bool:
-    (open_hour, close_hour) = 9, 20
+    (open_hour, close_hour) = 9, 16
     now = datetime.now()
     next_run = scheduler.get_job("spider").next_run_time
 
